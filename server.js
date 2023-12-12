948de8223078def9900f780cc5b78fc94ea7d75d
Emre <emre.surmeli@generalassemb.ly>
2023-12-12 16:22:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
 
+// import modules
+import express from 'express'
+
+// create express app
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// configurations (app.set)
+
+// middleware (app.use)
+app.use(express.json())
+
+// mount routes
+app.get('/', function(req, res) {
+    res.json({
+        message: 'Hii this my server'
+    })
+})
+
+app.get('/home', function(req, res) {
+    res.json({
+        message: 'Home Page'
+    })
+})
+
+app.post('/home', function(req, res) {
+    const body = req.body
+
+    res.json({
+        message: `Hi ${req.body.name}`
+    })
+})
+
+// Initialize server
+app.listen(PORT, function() {
+    console.log(`Server running at PORT ${PORT}`)
+})
